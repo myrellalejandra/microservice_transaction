95c7f15adb0d9c10745557e6ec79f176350db2cb
Myrella Alejandra Mas Bula <82183426+myrellalejandra@users.noreply.github.com>
2023-03-23 11:14:49 -0500

    Add files via upload

--- src/main/java/com/example/nttdata/controller/TransactionController.java
+++ src/main/java/com/example/nttdata/controller/TransactionController.java
@@ -1,7 +1,5 @@
 package com.example.nttdata.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,35 +13,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.nttdata.model.Transaction;
 import com.example.nttdata.service.TransactionService;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RestController
 @RequestMapping("/api")
 public class TransactionController {
-	
+
 	@Autowired
-	private TransactionService transactionService;
-	
-	@PostMapping("/postTransaction")
-	public void save(@RequestBody Transaction transaction) {
-		transactionService.save(transaction);
+	private TransactionService service;
+
+	@PostMapping("/transaction")
+	public Mono<Transaction> save(@RequestBody Transaction transaction) {
+		return service.save(transaction);
 	}
-	
+
 	@GetMapping("/transaction")
-	public List<Transaction> findAll(){
-		return transactionService.findAll();
+	public Flux<Transaction> findAll() {
+		return service.findAll();
 	}
-	
+
 	@GetMapping("/transaction/{id}")
-	public Transaction findById(@PathVariable String id){
-		return transactionService.findById(id).get();
+	public Mono<Transaction> findById(@PathVariable String id) {
+		return service.findById(id);
 	}
-	
+
 	@DeleteMapping("/transaction/{id}")
-	public void deleteById(@PathVariable String id){
-		transactionService.deleteById(id);
+	public void deleteById(@PathVariable String id) {
+		service.deleteById(id);
+	}
+
+	@PutMapping("/transaction")
+	public void update(@RequestBody Transaction transaction) {
+		service.save(transaction);
 	}
 	
-	@PutMapping("/transaction")
-	public void update(@RequestBody Transaction transaction){
-		transactionService.save(transaction);
+	//get transactions associated accountNumber
+	@GetMapping("/getAllByAccountNumber/{accountNumber}")
+	public Flux<Transaction> getAllByAccountNumber(@PathVariable Long accountNumber) {
+		return service.findByAccountNumber(accountNumber);
+	}
+	
+	//get transactions associated dni - client without account number
+	@GetMapping("/getAllByDni/{dni}")
+	public Flux<Transaction> getAllByDni(@PathVariable Long dni) {
+		return service.findByDni(dni);
 	}
 }
