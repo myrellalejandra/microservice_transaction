48519827118f1284d0b9fc4dd313136ef9fe6bf8
Myrella Alejandra Mas Bula <82183426+myrellalejandra@users.noreply.github.com>
2023-04-04 02:29:51 -0500

    Add files via upload
    .v4

--- src/main/java/com/example/nttdata/repository/TransactionRepository.java
+++ src/main/java/com/example/nttdata/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.example.nttdata.repository;
 
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.example.nttdata.model.Transaction;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @Repository
@@ -18,4 +20,14 @@ public interface TransactionRepository extends ReactiveMongoRepository<Transacti
 	  
 	  @Query(value="{ 'dni' : ?0 }")
 	  Flux<Transaction> getAllByDni(Long dni);
+	  
+	  @Aggregation("{ $match: { accountNumber: ?0 } }, { $sort: { date: -1 } }, { $limit: ?1 }")
+	  Mono<Transaction> getBalanceByAccount(Long accountNumber);
+	  
+	  //cant'use query if I'm going to use  a lot of filters
+	  @Aggregation("{ $match: { dni: ?0 } }, { $sort: { date: -1 } }, { $limit: ?1 }")
+	  Mono<Transaction> getBalanceByDni(Long dni);
+
+	  
+
 }
