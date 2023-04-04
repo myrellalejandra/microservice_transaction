8fe5b6c4a154fda4d9d10d43111af849b1fd7b41
Myrella Alejandra Mas Bula <82183426+myrellalejandra@users.noreply.github.com>
2023-04-04 02:32:35 -0500

    Add files via upload
    CLIENT RETROFIT

---
+++ retrofit_client/src/main/java/com/nttdata/demo/RetrofitClientApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrofitClientApplication.class, args);
+	}
+
+}
---
+++ retrofit_client/src/test/java/com/nttdata/demo/RetrofitClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
