04e47838aded63978b0450abccdadd7a8c51ce15
Myrella Alejandra Mas Bula <82183426+myrellalejandra@users.noreply.github.com>
2023-04-04 02:28:37 -0500

    Add files via upload
    .v4

--- src/main/java/com/example/nttdata/controller/TransactionController.java
+++ src/main/java/com/example/nttdata/controller/TransactionController.java
@@ -13,50 +13,62 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.nttdata.model.Transaction;
 import com.example.nttdata.service.TransactionService;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/transaction")
 public class TransactionController {
 
 	@Autowired
 	private TransactionService service;
 
-	@PostMapping("/transaction")
+	@PostMapping
 	public Mono<Transaction> save(@RequestBody Transaction transaction) {
 		return service.save(transaction);
 	}
 
-	@GetMapping("/transaction")
+	@GetMapping("/transactions")
 	public Flux<Transaction> findAll() {
 		return service.findAll();
 	}
 
-	@GetMapping("/transaction/{id}")
+	@GetMapping("/{id}")
 	public Mono<Transaction> findById(@PathVariable String id) {
 		return service.findById(id);
 	}
 
-	@DeleteMapping("/transaction/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteById(@PathVariable String id) {
 		service.deleteById(id);
 	}
 
-	@PutMapping("/transaction")
+	@PutMapping
 	public void update(@RequestBody Transaction transaction) {
 		service.save(transaction);
 	}
-	
-	//get transactions associated accountNumber
+
+	// get transactions associated accountNumber
 	@GetMapping("/getAllByAccountNumber/{accountNumber}")
 	public Flux<Transaction> getAllByAccountNumber(@PathVariable Long accountNumber) {
 		return service.findByAccountNumber(accountNumber);
 	}
-	
-	//get transactions associated dni - client without account number
+
+	// get transactions associated dni - client without account number
 	@GetMapping("/getAllByDni/{dni}")
 	public Flux<Transaction> getAllByDni(@PathVariable Long dni) {
 		return service.findByDni(dni);
+	}
+
+	@GetMapping("/getBalanceByAccount/{accountNumber}")
+	public Mono<Transaction> getBalanceByAccount(@PathVariable Long accountNumber) {
+		return service.getBalanceByAccount(accountNumber);
+	}
+
+	@GetMapping("/getBalanceByDni/{dni}")
+	public Mono<Transaction> getBalanceByDni(@PathVariable Long dni) {
+		return service.getBalanceByDni(dni).doOnNext(x -> x.toString());
 	}
 }
